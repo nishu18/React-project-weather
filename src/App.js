5ac76629050186866b63589d34605a6f935b789b
nishu18 <99903771+nishu18@users.noreply.github.com>
2022-05-21 19:36:35 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+// import Todo from "./component/todo-list";
+import Temp from "./component/weather/temp";
+
+
+const App=()=>{
+  
+
+  return  (
+    <div>
+      {/* <Todo /> */}
+      <Temp />
+    </div>
+  )
+};
+export default App;
+
